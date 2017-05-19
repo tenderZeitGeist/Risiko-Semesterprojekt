222fdea9c5558a7041be4c62529af815f22990a8
ABenjamins <deraikoben@gmail.com>
2017-05-19 08:27:52 +0200

    Added GUI class

---
+++ src/ui/PlayGroundGUI.java
@@ -0,0 +1,34 @@
+package ui;
+
+import domain.Risiko;
+import valueobjects.Player;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.swing.*;
+
+/**
+ * Created by YEAH BOIIIIIIIIIIIIIII on 19.05.2017.
+ */
+public class PlayGroundGUI {
+
+
+//    public PlayGroundGUI(String file) throws IOException {
+//
+//        risiko = new Risiko(file);
+//        in = new BufferedReader(new InputStreamReader(System.in));
+//    }
+
+    public static void main(String[] args) {
+        JFrame meinFrame = new JFrame("Beispiel JFrame");
+        /* Wir setzen die Breite und die Höhe
+           unseres Fensters auf 200 Pixel */
+        meinFrame.setSize(200, 200);
+        // Wir lassen unseren Frame anzeigen
+        meinFrame.setVisible(true);
+
+
+    }
+}
