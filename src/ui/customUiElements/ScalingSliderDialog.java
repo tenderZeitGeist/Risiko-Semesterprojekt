795b8395d37cd27f1b81be5d78965e9217191c4c
Sphrases <deraikoben@gmail.com>
2017-07-18 23:20:43 +0200

    some fiddeling

--- src/ui/customUiElements/ScalingSliderDialog.java
+++ src/ui/customUiElements/ScalingSliderDialog.java
@@ -13,13 +13,13 @@ public class ScalingSliderDialog {
 
         JOptionPane optionPane = new JOptionPane();
         JSlider slider = getSlider(optionPane);
-        optionPane.setMessage(new Object[] { "Select Scaling Percent: ", slider });
+        optionPane.setMessage(new Object[] { "select scaling percent: ",slider });
         optionPane.setMessageType(JOptionPane.QUESTION_MESSAGE);
         optionPane.setOptionType(JOptionPane.OK_CANCEL_OPTION);
 
-        JDialog dialog = optionPane.createDialog(parent, "Select Scaling");
+        JDialog dialog = optionPane.createDialog(parent, "select scaling");
         dialog.setVisible(true);
-        System.out.println("Input: " + optionPane.getInputValue());
+        System.out.println("scaling: " + optionPane.getInputValue());
         if(optionPane.getInputValue() == null){
             System.exit(0);
             return 666;
@@ -31,15 +31,17 @@ public class ScalingSliderDialog {
 
     static JSlider getSlider(final JOptionPane optionPane) {
         JSlider slider = new JSlider(0, 100, 70);
-        slider.setMinorTickSpacing(1);
-        slider.setMajorTickSpacing(50);
-        slider.setPaintTicks(false);
+        slider.setMinorTickSpacing(10);
+        slider.setMajorTickSpacing(20);
+        slider.setPaintTicks(true);
         slider.setPaintLabels(true);
+        slider.setSnapToTicks(true);
         ChangeListener changeListener = new ChangeListener() {
             public void stateChanged(ChangeEvent changeEvent) {
                 JSlider theSlider = (JSlider) changeEvent.getSource();
                 if (!theSlider.getValueIsAdjusting()) {
                     optionPane.setInputValue(new Integer(theSlider.getValue()));
+
                 }
             }
         };
