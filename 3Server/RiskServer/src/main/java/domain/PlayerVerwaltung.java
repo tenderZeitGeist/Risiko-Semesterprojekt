4914ed464e46a6652ae491fd0e287be62d3f61bb
Unknown <zeit_geist@posteo.de>
2017-09-11 15:31:38 +0200

    didndu nuffin

--- 3Server/RiskServer/src/main/java/domain/PlayerVerwaltung.java
+++ 3Server/RiskServer/src/main/java/domain/PlayerVerwaltung.java
@@ -15,8 +15,8 @@ public class PlayerVerwaltung {
 
 
     private PersistenceManager pm = new FilePersistenceManager();
-    private Vector < Player > playerList = new Vector<Player>();
-    private  Player currentPlayer;
+    private Vector<Player> playerList = new Vector<Player>();
+    private Player currentPlayer;
 
     public void createPlayer(int newPlayerID, String newPlayerName) throws PlayerAlreadyExistsException {
         Player newPlayer = new Player(newPlayerID, newPlayerName);
@@ -26,11 +26,11 @@ public class PlayerVerwaltung {
         currentPlayer = playerList.get(0);
     }
 
-    public Vector < Player > getPlayerList() {
+    public Vector<Player> getPlayerList() {
         return playerList;
     }
 
-    public void setPlayerList( Vector < Player > p) {
+    public void setPlayerList(Vector<Player> p) {
         this.playerList = p;
     }
 
@@ -43,33 +43,43 @@ public class PlayerVerwaltung {
         this.currentPlayer = playerList.get(nextPlayer);
     }
 
-    public void setPlayerIDs(){
+    public void setPlayerIDs() {
         int playerID = 0;
         for (Player p : playerList) {
-            p.setPlayerID ( playerID++ );
+            p.setPlayerID(playerID++);
         }
     }
 
     public void serializePlayers(Player p) throws IOException {
 
         //Vector<Player> tempPlayerList1 = new Vector<>(playerList);
+
         Vector<Player> tempPlayerList = new Vector<>();
+        int position = playerList.indexOf(p);
 
-        int playerNumber = p.getPlayerID();
-
-        for (int i = 0; i < playerList.size(); i++) {
-            if (i > playerNumber) {
-            //    tempPlayerList1.get(i).setPlayerID(newPlayerID++);
-                tempPlayerList.add(playerList.get(i));
-            }
+        for (int i = position + 1; i < playerList.size(); i++) {
+            tempPlayerList.add(playerList.get(i));
         }
 
-        for (int i = 0; i < playerList.size(); i++) {
-            if (i <= playerNumber) {
-            //    tempPlayerList1.get(i).setPlayerID(newPlayerID++);
-                tempPlayerList.add(playerList.get(i));
-            }
+        for (int j = 0; j > position; j++) {
+            tempPlayerList.add(playerList.get(j));
         }
+
+//int playerNumber = p.getPlayerID();
+
+//for (int i = 0; i < playerList.size(); i++) {
+//    if (i > playerNumber) {
+//    //    tempPlayerList1.get(i).setPlayerID(newPlayerID++);
+//        tempPlayerList.add(playerList.get(i));
+//    }
+//}
+
+//for (int i = 0; i < playerList.size(); i++) {
+//    if (i <= playerNumber) {
+//    //    tempPlayerList1.get(i).setPlayerID(newPlayerID++);
+//        tempPlayerList.add(playerList.get(i));
+//    }
+//}
 
         try (FileOutputStream fos = new FileOutputStream("player.ser");
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
